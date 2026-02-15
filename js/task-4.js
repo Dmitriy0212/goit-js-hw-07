be4ec9ac59c35607f02a3a81b3ad921b30a569cd
Dmitriy0212 <demarr68@gmail.com>
2026-02-15 14:27:36 +0200

    fix

--- js/task-4.js
+++ js/task-4.js
@@ -1,7 +1,6 @@
 const registerForm = document.querySelector(".login-form");
 
 registerForm.addEventListener("submit", handleSubmit);
-
 function handleSubmit(event) {
   event.preventDefault();
   const form = event.target;
