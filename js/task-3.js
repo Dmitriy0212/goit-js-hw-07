6daf0c26a6ff49df0c5dae333bcd1f964842bba2
Dmitriy0212 <demarr68@gmail.com>
2026-02-15 12:31:49 +0200

    fix

--- js/task-3.js
+++ js/task-3.js
@@ -3,9 +3,6 @@ const output = document.querySelector("#name-output");
 
 textInput.addEventListener("input", (event) => {
   output.textContent = event.currentTarget.value.trim();
-});
-textInput.addEventListener("blur", (event) => {
-  output.textContent = event.currentTarget.value.trim();
   if (output.textContent === "") {
     output.textContent = "Anonymous";
   }
